e620dd245d9b1a672f86958f7b6758c23da4b776
Abhishek Pathak <69761436+scorcism@users.noreply.github.com>
2021-10-04 10:55:11 +0530

    Selection sort

--- Java/Main.java
+++ Java/Main.java
@@ -1,33 +1,39 @@
 package com.abhishek;
 
 import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	int[] arr = {3};
-    bubble(arr);
+	int[] arr = {3,2,653,23,4};
+    selection(arr);
         System.out.println(Arrays.toString(arr));
     }
-    static void bubble(int[] arr){
-        boolean swapped;
-//        run the steps n-1 times
-        for (int i = 0; i < arr.length; i++) {
-            swapped = false;
-//            for each step, max item will come at the last respective index.
-            for (int j = 1; j < arr.length - i; j++) {
-//                swap if the item smaller than the previous item
-                if(arr[j]<arr[j-1]){
-                    int tmp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = tmp;
-                    swapped = true;
-                }
-            }
-//            if you didn't swap for a particular value of i ,
-//            it mens the array is sorted. Hence, stop the program.
-            if(!swapped){
-                break;
+
+    static void selection(int[] arr){
+        for (int i=0; i < arr.length;i++){
+//            find the max item in the remaining array and swap with correct index.
+            int last =arr.length-i-1;
+//            this will return the index of the largest number of the array. Here we are passing
+//            1st element and the last element of the array.
+            int maxIndex=  getMaxIndex(arr,0,last);
+//            swapping the two element, largest indexed element n our last element.
+            swap(arr,maxIndex,last);
+        }
+    }
+    static int getMaxIndex(int[] arr, int start, int end){
+//        Here we have assumed that the highest element is at the start.
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if(arr[max] < arr[i]){
+                max = i;
             }
         }
+        return max;
+    }
+    static void swap (int[] arr, int first , int second){
+        int temp = arr[first];
+        arr[first]  = arr[second];
+        arr[second] = temp;
     }
 }
