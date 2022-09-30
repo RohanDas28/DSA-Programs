5dc5ca2a4c5ccc63441ddd252799df8c4f325347
The Crazy Coder <55174974+TheCrazyCoder28@users.noreply.github.com>
2022-10-01 01:14:16 +0530

    Create dist.py

---
+++ misc/dist.py
@@ -0,0 +1,12 @@
+
+x1=int(input("enter x1 : "))
+
+x2=int(input("enter x2 : "))
+
+y1=int(input("enter y1 : "))
+
+y2=int(input("enter y2 : "))
+
+result= ((((x2 - x1 )**2) + ((y2-y1)**2) )**0.5)
+
+print("distance between",(x1,x2),"and",(y1,y2),"is : ",result)
